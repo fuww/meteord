f58865c391b1a2042bcba88b82691358de7f5d3b
Joost van der Laan <joost@fashionunited.com>
2016-02-16 08:48:53 +0100

    fashionunited-extras

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -3,4 +3,5 @@ set -e
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
+bash $METEORD_DIR/lib/fuww_install_extras.sh
 bash $METEORD_DIR/lib/cleanup.sh
---
+++ base/scripts/lib/fuww_install_extras.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+apt-get install -y python-pip
+pip install docker-cloud
+npm install -g eslint
+npm install -g forever
